8fd5c1fef2e1e8f5a526c0aeeb3ca37087a7ce2c
Alvaro Echevarria Cuesta <alvaroecuesta@gmail.com>
2025-10-14 11:53:52 -0400

    init

---
+++ src/manifest.ts
@@ -0,0 +1,17 @@
+import { defineManifest } from "@crxjs/vite-plugin";
+
+export default defineManifest({
+    manifest_version: 3,
+    version: "1.0",
+    name: "Echo Extension Template",
+    description: "Template to start an echo chrome extension with the proper auth to use AI resources like OpenAI, Anthropic, and more.",
+    host_permissions: ["<all_urls>"],
+    background: {
+        service_worker: "src/background.ts",
+        type: "module",
+    },
+    action: {
+        default_popup: "src/echo-popup/index.html",
+    },
+    permissions: ["identity", "storage", "contextMenus"],
+})
---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite";
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { crx } from "@crxjs/vite-plugin";
+import manifest from "./src/manifest";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), crx({ manifest })],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        popup: "src/echo-popup/index.html",
+      },
+    },
+    outDir: "dist",
+  },
+});
