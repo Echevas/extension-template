90bd7d2ce4aca40805776cec810da37a5ac9b062
Alvaro Echevarria Cuesta <alvaroecuesta@gmail.com>
2025-10-14 15:27:10 -0400

    handle just sign in and sign out

--- src/echo-popup/src/pages/welcome.tsx
+++ src/echo-popup/src/pages/welcome.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import { EchoSignin } from "@/components/ui/echo-sign-in";
-import { useEcho } from "@/hooks/useEcho";
 
 export const WelcomePage: React.FC = () => {
-  const { isAuthenticated } = useEcho();
 
   return (
     
     <div className="flex flex-col h-full w-full items-center justify-center gap-4">
-      {isAuthenticated ? <div>Signed in</div> : <EchoSignin className="w-32 h-12 mb-4" />}
+      <EchoSignin className="w-32 h-12 mb-4" />
     </div>
   );
 };
